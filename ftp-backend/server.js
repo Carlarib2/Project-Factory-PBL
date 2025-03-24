7e3d17bc50df39bc1ae58a8761c68e3b7fb4aa60
tiagodccosta <tiagocunhacosta@gmail.com>
2025-03-24 18:44:14 +0000

    Starting the Server

--- ftp-backend/server.js
+++ ftp-backend/server.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
+
 const port = process.env.PORT;
 
 // Middleware
